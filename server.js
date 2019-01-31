f25359b9c47596bd1663c7d15a80702625469084
davide <davide.dantonio@webeetle.com>
2019-01-31 16:34:42 +0100

    enh

--- server.js
+++ server.js
@@ -1,23 +1,55 @@
+#! /usr/bin/env node
+
 'use strict'
 
 const assert = require('assert')
+const parseArgs = require('./args')
+const fs = require('fs')
+const path = require('path')
+const resolveFrom = require('resolve-from')
 
 let Fastify = null
+let fastifyPackageJSON = null
+
+function showHelp () {
+  console.log(fs.readFileSync(path.join(__dirname, 'help', 'start.txt'), 'utf8'))
+  return module.exports.stop()
+}
+
+function stop (error) {
+  if (error) {
+    console.error(error)
+    process.exit(1)
+  }
+  process.exit()
+}
 
 function loadModules (opts) {
   try {
-    const basedir = path.resolve(process.cwd(), opts._[0])
+    const basedir = path.resolve(process.cwd(), opts.file)
 
     Fastify = require(resolveFrom.silent(basedir, 'fastify') || 'fastify')
-    fastifyPackageJSON = require(resolveFrom.silent(basedir, 'fastify/package.json') || 'fastify/package.json')
   } catch (e) {
     module.exports.stop(e)
   }
 }
 
+function start (args, cb) {
+  let opts = parseArgs(args)
+
+  if (!fs.existsSync(opts.file)) {
+    console.error("Missing the required file app.js\n")
+    return showHelp()
+  }
+
+  loadModules(opts)
+  return runFastify(args, cb)
+}
+
 function runFastify (args, cb) {
   require('dotenv').config()
   let opts = parseArgs(args)
+
   opts.port = opts.port || process.env.PORT || 3000
   cb = cb || assert.ifError
 
@@ -25,10 +57,20 @@ function runFastify (args, cb) {
 
   var file = null
   try {
-    file = require('./app.js')
+    file = require(path.resolve(process.cwd(), opts.file))
   } catch (e) {
     return module.exports.stop(e)
   }
+
+  console.log(file.constructor.name)
 }
 
-module.exports = runFastify
+function cli (args) {
+  start(args)
+}
+
+module.exports = { start, runFastify, stop }
+
+if (require.main === module) {
+  cli(process.argv.slice(2))
+}
